c3696566d180e89b1e84dfc7d0408a59bdd77121
Higor Costa <higorcosta972@gmail.com>
2023-01-22 08:49:35 -0300

    Importa função servicoAdicional

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,10 @@ import validacaoForm from "./modules/validacao-form.js";
 import trocaSecao from "./modules/troca-secao.js";
 import escolhaPlano from "./modules/escolha-plano.js";
 import planoMensalAnual from "./modules/mensal-anual.js";
+import servicoAdicional from "./modules/servico-adicional.js";
 
 validacaoForm();
 trocaSecao();
 escolhaPlano();
 planoMensalAnual();
+servicoAdicional();

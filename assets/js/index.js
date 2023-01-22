ad9db314888a5495f5b38c70d62d3df00b0ecb16
Higor Costa <higorcosta972@gmail.com>
2023-01-21 22:46:29 -0300

    Importa função escolhaPlano

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 import validacaoForm from "./modules/validacao-form.js";
 import trocaSecao from "./modules/troca-secao.js";
+import escolhaPlano from "./modules/escolha-plano.js";
 
 validacaoForm();
 trocaSecao();
+escolhaPlano();

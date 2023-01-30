b741968ed1ba663bf28cb968d987a5c3e484cc23
Higor Costa <higorcosta972@gmail.com>
2023-01-30 15:34:43 -0300

    Importa função verificacaoConfirmacao

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,11 @@ import trocaSecao from "./modules/troca-secao.js";
 import escolhaPlano from "./modules/escolha-plano.js";
 import planoMensalAnual from "./modules/mensal-anual.js";
 import servicoAdicional from "./modules/servico-adicional.js";
+import verificacaoConfirmacao from "./modules/finalizar.js";
 
 validacaoForm();
 trocaSecao();
 escolhaPlano();
 planoMensalAnual();
 servicoAdicional();
+verificacaoConfirmacao();

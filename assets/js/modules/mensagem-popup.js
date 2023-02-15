c78bd3859ff0d89ed0112c20e696502b8f3aa538
Higor Costa <higorcosta972@gmail.com>
2023-02-15 14:56:30 -0300

    Docs: adiciona documentação da função mensagemPopup

--- assets/js/modules/mensagem-popup.js
+++ assets/js/modules/mensagem-popup.js
@@ -5,16 +5,19 @@ export default function mensagemPopup() {
     const botaoNext = document.querySelector('.next');
     
     function controleExibicaoPopup() {
+      // Se o this for o icone de exclamação, ele será escondido e o pop-up será exibido
       if(this.dataset.popup === "abrir") {
         popup.classList.add("ativo");
         this.classList.remove('ativo');
       } 
+      // Se o this for o icone do X, o pop-up será escondido e o icone de exclamação será exibido
       else if (this.dataset.popup === "fechar") {
         popup.classList.remove("ativo");
         iconeExclamacao.classList.add('ativo');
       }
     
       setTimeout(() => {
+        // Caso a função tenha sido chamada pelo botão 'Next Step' e o pop-up esteja visivel, pop-up será escondido, 3s 
         if (this === botaoNext && popup.classList.contains("ativo")) {
           popup.classList.remove("ativo");
           iconeExclamacao.classList.add('ativo');
@@ -23,7 +26,7 @@ export default function mensagemPopup() {
     }
     
     controlesPopup.forEach(controle => {
-        controle.addEventListener('click', controleExibicaoPopup)
+        controle.addEventListener('click', controleExibicaoPopup);
     });
     
     botaoNext.addEventListener('click', controleExibicaoPopup);

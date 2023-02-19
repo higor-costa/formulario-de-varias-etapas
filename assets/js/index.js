242fa6f01a04026aa4374fcdda9e2bc5495e1a92
Higor Costa <higorcosta972@gmail.com>
2023-02-19 12:47:38 -0300

    Conserta src de arquivos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
-import validacaoForm from "./modules/validacao-form.js";
-import trocaSecao from "./modules/troca-secao.js";
-import escolhaPlano from "./modules/escolha-plano.js";
-import planoMensalAnual from "./modules/mensal-anual.js";
-import servicoAdicional from "./modules/servico-adicional.js";
-import verificacaoConfirmacao from "./modules/finalizar.js";
-import exibicaoBotoes from "./modules/exibicao-botoes.js";
-import mensagemPopup from "./modules/mensagem-popup.js";
+import validacaoForm from "../js/modules/validacao-form.js";
+import trocaSecao from "../js/modules/troca-secao.js";
+import escolhaPlano from "../js/modules/escolha-plano.js";
+import planoMensalAnual from "../js/modules/mensal-anual.js";
+import servicoAdicional from "../js/modules/servico-adicional.js";
+import verificacaoConfirmacao from "../js/modules/finalizar.js";
+import exibicaoBotoes from "../js/modules/exibicao-botoes.js";
+import mensagemPopup from "../js/modules/mensagem-popup.js";
 
 validacaoForm();
 trocaSecao();
